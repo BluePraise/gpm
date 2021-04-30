ac45521db938c06b7e1ce01162d083b706ea2fa7
Magalie Chetrit <magaliechetrit@gmail.com>
2021-04-30 16:38:22 +0200

    added visuals, added nav and changed color

--- front-page.php
+++ front-page.php
@@ -4,18 +4,78 @@
 */
 
 get_header(); ?>
-<main class="page-content">
+
 <div class="hero">
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo-installatie.jpg" alt="Logo GPM Installatietechniek">
 </div>
+<main class="page-content">
 <div class="services">
     <h2>Wat doen wij?</h2>
-    <p>Welkom op onze website. GPM bouw en techniek, voor bouwen, verbouwen en renoveren van kleine en middelgrote projecten. 
-Wij maken graag mooie dingen. Ontzorgen van onze klanten, dat is wat wij belangrijk vinden. 
-Op deze website kunt u nader kennismaken met onze diensten. Mocht u behoefte hebben aan meer informatie, neem dan contact met ons op. 
-Graag bespreken wij uw wensen en onze mogelijkheden. 
-</p>
+    <p>Welkom op de website van GPM Installatietechniek. Als zelfstandig werkend elektrotechnisch installatiebureau bestaan onze dagelijkse bezigheden uit het aanleggen van elektra, verlichting, intercom, audio/video, zonnepanelen en domotica installaties en natuurlijk het oplossen van storingen. </p>
+    <h3>Ook voor nieuwbouw en renovatie</h3>
+    <p>Bij nieuwbouw projecten zijn wij vaak de eersten die komen voor de aanleg van de infrastructuur en zijn wij tevens de laatsten die weggaan na het monteren van verlichting, schakelaars, wandcontactdozen en dergelijke.
+    Bij renovatieprojecten adviseren wij de klanten in de mogelijkheden die er zijn en spelen goed in op hun wensen. Oplossingen kunnen onder meer gevonden worden in bijvoorbeeld domotica welke tot een van onze specialiteiten behoort.</p>
 </div>
+<section class="chapter electro">
+    <div class="tag-line">
+        <h2>Elektrotechnische installaties van hoge kwaliteit</h2>
+        <p>De meeste werkzaamheden van GPM Installatietechniek bestaan uit werkzaamheden aan de laagspanningsinstallatie. Dit is onder te verdelen in drie deelgebieden:</p>
+    </div>
+    <div class="grid container col-3">
+        <div class="col-1">
+            <h3>Onderhoud</h3>
+            <p>U kunt bij ons terecht voor alle denkbare werkzaamheden in en om de woning, winkel of kantoorruimte.
+            Hierbij kunt u denken aan het verplaatsen/vervangen van wandcontactdozen, schakelaars, het bijmaken van spotjes en andere aansluitpunten of voor het oplossen van storingen aan de elektrotechnische installatie.
+            </p>
+        </div>
+        <div class="col-1">
+            <h3>Renovatiebouw</h3>
+            <p>Bij renovatiebouw denkt GPM Installatietechniek graag mee om de verouderde installaties aan te passen, zodat zij voldoen aan de huidige eisen die gesteld worden. Uiteraard worden de nieuwste ontwikkelingen hierin meegenomen. 
+            </p>
+        </div>
+        <div class="col-1">
+            <h3>Nieuwbouw</h3>
+            <p>Zelfs wanneer u een kavel aankoopt en u wilt daar een woning of utiliteitsgebouw op bouwen kan GPM Installatietechniek u helpen met de elektrotechnische installatie. Wij willen graag met u en/of de architect meedenken om tot een oplossing te komen die perfect is afgestemd op u en uw wensen. 
+            </p>
+        </div>
+    </div>
+</section>
+<section class="chapter domotica grid col-2">
+    <figure class="filter-grey"><img src="<?php echo get_stylesheet_directory_uri();?>/img/visuals/domotica.jpg" alt=""></figure>
+    <div class="tag-line">
+        <h2>Domotica</h2>
+        <p>De vraag naar gebouwbeheersystemen neemt voortdurend toe.
+Of het nu om een eengezinswoning of een kantorencomplex gaat, de vraag naar comfort en flexibiliteit voor het beheer van verlichting, temperatuurregeling, luchtbehandeling, audio/video en toegangscontrole neemt toe. Gelijktijdig wordt het efficiënter gebruik van energie steeds belangrijker.
+</p>
+<p>
+    Meer comfort en veiligheid gekoppeld aan een lager energieverbruik kan enkel met intelligente besturings- en controlesystemen. Om de gegevensoverdracht van en naar alle componenten van het gebouwenautomatiserings-systeem te verzekeren, is er een systeem nodig dat het probleem van geïsoleerde subsystemen oplost door er voor te zorgen dat al deze componenten met elkaar kunnen communiceren: ze als het ware dezelfde ‘taal’ te laten spreken.
+</p>
+    </div>
+</section>
+<section class="chapter brandinstallatie grid col-2">
+    <div class="tag-line">
+        <h2>Brandmeld- en ontruimingsinstallaties voor een veilige omgeving</h2>
+        <p>Aan de veiligheid in bijeenkomstgebouwen of gebouwen met een publieke functie worden steeds strengere eisen gesteld. Belangrijk onderdeel daarin is de ontruiming van een gebouw.</p>
+        <p>Wanneer moet er ontruimd worden? Hierin biedt een brandmeldinstallatie uitkomst. De brandmeldinstallatie geeft alarm door het automatisch detecteren van rook/hitte of door het indrukken van een handmelder. Wanneer er aan een of meer van deze voorwaarden is voldaan zal er een ontruimingssignaal afgaan welke in elke ruimte te horen is. In welke richting er gevlucht dient te worden is te zien aan de noodverlichting. Deze wordt gekenmerkt door de groene afbeeldingen met het vluchtende poppetje hierop afgebeeld. Bovendien zorgt deze verlichting ervoor dat er altijd minimaal 1,5 lux aanwezig is op de vluchtweg.</p>
+    </div>
+    <figure class="filter-grey"><img src="<?php echo get_stylesheet_directory_uri();?>/img/visuals/fire-safety.jpg" alt=""></figure>
+</section>
+<section class="chapter zonnepanelen grid col-2">
+    <figure class="filter-grey"><img src="<?php echo get_stylesheet_directory_uri();?>/img/visuals/solar-panel.jpg" alt=""></figure>
+    <div class="tag-line">
+        <h2>Zonnepanelen</h2>
+        <p>Zonnepanelen zijn een duurzame investering voor uw huis of bedrijf. Een investering in zonnepanelen verhoogt direct de waarde van uw huis of bedrijfspand. Uw woning of bedrijf krijgt een beter energielabel en hoe energiezuiniger de woning/bedrijfspand, hoe aantrekkelijker voor toekomstige kopers.
+            Neem contact met ons op voor meer informatie.</p>
+    </div>
+</section>
+<section class="chapter camera-security grid col-2">
+    <div class="tag-line">
+        <h2>Camerabeveiliging tot in detail geregeld</h2>
+        <p>U laat ons uw wensen weten als wij bij u langs komen om de situatie te bekijken. U kunt bijvoorbeeld camerabewaking willen op een parkeerplaats, bij een woonhuis of bij een winkelpand. Voor iedere situatie zijn er verschillende toepassingen mogelijk. U kunt er voor kiezen om de camera’s heel opvallend te plaatsen zodat ze ook afschrikken, of u kiest ervoor om ze discreet op te hangen.</p>
+    </div>
+    <figure class="filter-grey"><img src="<?php echo get_stylesheet_directory_uri();?>/img/visuals/security.jpg" alt=""></figure>
+</section>
+
 <div class="contact-block">
     <h2> <i data-feather="edit-2"></i><span>Contact Ons</span></h2>
     <ul>
@@ -26,18 +86,42 @@ Graag bespreken wij uw wensen en onze mogelijkheden.
     </ul>
 </div>
 <div class="usp">
-    <ul>
-        <li>
-            <img src="<?php echo get_stylesheet_directory_uri();?>/img/service-icon.png">
-            <span>Service</span>
-        </li>
-        <li><img src="<?php echo get_stylesheet_directory_uri();?>/img/veiligheid-icon.png">
-            <span>100% Veiligheid</span></li>
-        <li><img src="<?php echo get_stylesheet_directory_uri();?>/img/vakmensen-icon.png">
-            <span>Vakmensen</span></li>
-        <li><img src="<?php echo get_stylesheet_directory_uri();?>/img/contact-icon.png">
-            <span>Snel Contact</span></li>
-    </ul>
+    <div class="usp-list">
+        <div class="usp-list-item">
+            <div class="usp-list-item-header">
+                <img src="<?php echo get_stylesheet_directory_uri();?>/img/service-icon.png">
+                <span>Advies op Maat</span>
+            </div>
+            <p>
+                Bij GPM Installatietechniek bent u aan het juiste adres voor oplossingen bij brede en specialistische vraagstukken voor installaties en domotica.
+            </p>
+        </div>
+        <div class="usp-list-item">
+            <div class="usp-list-item-header">
+                <img src="<?php echo get_stylesheet_directory_uri();?>/img/veiligheid-icon.png">
+                <span>Gecertificeerd</span>
+            </div>
+            <p>
+                Wij profileren ons als een betrouwbaar en goed bedrijf dat uitsluitend kwaliteit nastreeft.
+            </p>
+
+        </div>
+        <div class="usp-list-item">
+            <div class="usp-list-item-header">
+                <img src="<?php echo get_stylesheet_directory_uri();?>/img/vakmensen-icon.png">
+                <span>Vakkundig Specialist</span>
+            </div>
+            <p>Uw specialist in elektrotechniek en breed inzetbaar voor uw project, waarbij kwaliteit hoog in het vaandel staat bij het opleveren van de installatie. Wij denken graag met u mee en adviseren waar nodig. </p>
+        </div>    
+
+        <div class="usp-list-item">
+            <div class="usp-list-item-header">
+                <img src="<?php echo get_stylesheet_directory_uri();?>/img/contact-icon.png">
+                <span>Kwaliteit</span>
+            </div>
+            <p>GPM Installatietechniek stelt eisen aan de installaties en de kwaliteitsbewaking. U bent zeker van de kwaliteit van onze installaties.</p>
+        </div>
+    </div>
 </div>
 
 </main>
--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 		<?php endif; ?>
         <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/favicon.ico" type="image/x-icon" /><link rel="preconnect" href="https://fonts.gstatic.com">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200&family=Raleway&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;900&family=Raleway:wght@200;600;900&display=swap" rel="stylesheet">
         <script src="https://unpkg.com/feather-icons"></script>
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 		<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/a214b22300f3656fb3ad76337/6dfbd0aa9b3c8e4c8f93fa98a.js");</script>
@@ -23,5 +23,6 @@
 		
 
 <header>
-
+	<div class="d-inline-block top-bar-item"><i class="d-inline-block" data-feather="phone"></i><a class="d-inline-block" href="tel:+31 26 213 47 58">+31 26 213 47 58</a></div>
+	<div class="d-inline-block top-bar-item"><li><i class="d-inline-block" data-feather="mail"></i><a class="d-inline-block" href="mailto:info@gpminstallatietechniek.nl" target="_blank" rel="noopener">info@gpminstallatietechniek.nl</a></li></div>
 </header>
